8b3e0f355b79b4eef2290921bda825704050491d
Sharon <sharon9476@gmail.com>
2024-03-15 07:34:35 +0200

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
-      Hello World
-    </main>
+    <div className="h-screen">
+      <UserButton />
+      <div className="space-x-4">
+        <Button asChild>
+          <Link href={"/sign-up"}>Sign Up</Link>
+        </Button>
+        <Button asChild>
+          <Link href={"/sign-in"}>Sign In</Link>
+        </Button>
+      </div>
+    </div>
   );
 }
